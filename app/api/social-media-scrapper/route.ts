8aa4034ccf32ba5a871389eb1eee30abca31cc4c
houssam15 <houssam.messiaze123@gmail.com>
2024-05-30 11:43:56 +0100

    h

---
+++ app/api/social-media-scrapper/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+
+
+export async function GET (req:NextRequest){
+
+    try {     
+
+      return NextResponse.json({message: 'User created successfully'},{status: 200});
+    } catch (error) {
+      return NextResponse.json({error: 'Internal Server Error'}, {status: 500});
+    }
+} 
+
+
