e5187259c9cae09e70a0a7c89df6aa39906fbb55
houssam15 <houssam.messiaze123@gmail.com>
2024-06-04 23:43:57 +0100

    h

---
+++ app/competitor_analysis_scraper/services/getTotalPages.ts
@@ -0,0 +1,21 @@
+import puppeteer from "puppeteer";
+interface getTotalPagesParams {
+    url : Required<string>;
+}
+
+export default async function getTotalPages({url} : getTotalPagesParams){
+    try{
+        const b = await puppeteer.launch();
+        // const p = await b.newPage();
+        // await p.goto(url, { waitUntil: 'networkidle0' });
+        // const pc = await p.evaluate(() => {
+        // return document.getElementsByTagName('a').length;
+        // });
+        // await b.close();
+        // return pc;
+    }catch(err){
+        console.error(err);
+        return -1;
+    }
+    return 0;
+}
