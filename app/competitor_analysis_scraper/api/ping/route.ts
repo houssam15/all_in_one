4903d470ba95770bd4728e752c698104f0466e11
houssam15 <houssam.messiaze123@gmail.com>
2024-06-04 21:19:17 +0100

    h

---
+++ app/competitor_analysis_scraper/api/ping/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
+//http://localhost:3000/competitor_analysis_scraper/api/ping
+export async function GET (req:NextRequest){
+      return NextResponse.json({status: 'Ok',method:"Ping"},{status: 200});
+} 
+
+
