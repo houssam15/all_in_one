247e19d6080e9401daf7b79850b47f6709794f8a
houssam15 <houssam.messiaze123@gmail.com>
2024-06-08 11:22:17 +0100

    'h'

--- components/sideNar.tsx
+++ components/sideNar.tsx
@@ -22,7 +22,7 @@ export default function SideNar(/*{setMainContent}:SideNavPropsType*/) {
         <nav>
     
           <SideNavList />
-          <a
+          {/* <a
             className="flex items-center px-4 py-2 mt-5 text-gray-200 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
             href="#"
           >
@@ -58,7 +58,7 @@ export default function SideNar(/*{setMainContent}:SideNavPropsType*/) {
           >
             <CiCircleQuestion size={25} />
             <span className="mx-4 font-medium">Settings</span>
-          </a>
+          </a> */}
         </nav>
         <a href="#" className="flex items-center px-4 -mx-2">
           <img
--- components/sideNavList.tsx
+++ components/sideNavList.tsx
@@ -1,41 +1,46 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { MenuType } from "../config/type";
 import axios from "axios";
 import "@fortawesome/fontawesome-free/css/all.css";
+import { MenuType } from "../config/type";
 
-//dépanage
+// Dépannage
 export default function SideNavList() {
   const [menu, setMenu] = useState<MenuType[]>([]);
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMenu = async () => {
       try {
         const response = await axios.get("/modules/api/menu");
-        console.log(response);
         setMenu(response.data.data.menu);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMenu();
   }, []);
 
-  return !isError ? (
-    menu.map((elm) => (
+  return isLoading ? (
+    // Add loading indicator here
+    <div className="text-white">Loading...</div>
+  ) : !isError ? (
+    menu.filter(elm => elm.active == true ).map((elm) => (
       <Link
         key={elm.route}
         className="flex items-center px-4 py-2 mt-5 text-gray-200 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
         href={elm.route}
       >
         <i className={`${elm.icon} text-lg`} />
-        <span className="mx-2 font-bold text-sm">{elm.name}</span>
+        <span className="mx-2 font-bold text-xs">{elm.name}</span>
       </Link>
     ))
   ) : (
-    <>error</>
+    <div className="text-white">Error</div>
   );
 }
