9fa0c7a5db043440a5eacd15e6dbcd9dea663d8b
houssam15 <houssam.messiaze123@gmail.com>
2024-06-08 11:59:42 +0100

    new update

--- auto_git_script.js
+++ auto_git_script.js
@@ -4,31 +4,34 @@ const { exec } = require("child_process");
 const args = process.argv.slice(2);
 const message = args.length > 0 ? args[0] : "new update";
 
-const commands = [
-  { command: "git add .", label: "add" },
-  { command: `git commit -m "${message}"`, label: "commit" },
-  { command: "git pull", label: "pull" },
-  { command: "git merge", label: "merge" },
-  { command: "git push", label: "push" }
-];
+const executeCommands = async () => {
+  const commands = [
+    { command: "git add .", label: "add", color: "blue" },
+    { command: `git commit -m "${message}"`, label: "commit", color: "green" },
+    { command: "git pull", label: "pull", color: "yellow" },
+    { command: "git merge", label: "merge", color: "cyan" },
+    { command: "git push", label: "push", color: "magenta" }
+  ];
 
-const executeCommands = async (commands) => {
-  for (let { command, label } of commands) {
+  for (let { command, label, color } of commands) {
     console.log(`=====================${label}=========================`);
+    const chalk = await import("chalk");
+    const colorFunction = chalk.default[color];
+    console.log(colorFunction(`=====================${label}=========================`));
     await new Promise((resolve, reject) => {
       exec(command, (err, stdout, stderr) => {
         if (err) {
-          console.error(`Error: ${err.message}`);
+          console.error(colorFunction(`Error: ${err.message}`));
           reject(err);
           return;
         }
         if (stderr) {
-          console.error(`Stderr: ${stderr}`);
+          console.error(colorFunction(`Stderr: ${stderr}`));
         }
         if (stdout) {
-          console.log(`Stdout: ${stdout}`);
+          console.log(colorFunction(`Stdout: ${stdout}`));
         } else {
-          console.log(`${label} command executed successfully.`);
+          console.log(colorFunction(`${label} command executed successfully.`));
         }
         resolve();
       });
@@ -36,7 +39,7 @@ const executeCommands = async (commands) => {
   }
 };
 
-executeCommands(commands)
+executeCommands()
   .then(() => {
     console.log("All commands executed successfully.");
   })
