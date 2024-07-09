0e96891ce62b8ee0fe4ec82ecb153c4fd999ff81
houssam15 <houssam.messiaze123@gmail.com>
2024-07-09 23:20:22 +0100

    new update

--- app/competitor-analysis-scraper/api/cron/start/route.ts
+++ app/competitor-analysis-scraper/api/cron/start/route.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //http://localhost:3000/competitor-analysis-scraper/api/cron/start
 export async function GET (req:NextRequest){
-    const result = await axios.get(process.env.COMPETITOR_ANALYSIS_SCRAPPER_CRON_HOST + "/api/competitor-analysis-scraper/start-cron");
-    if(result.data.status=="KO") return NextResponse.json({errors:["Already running !"] },{status: 400});
+    //const result = await axios.get(process.env.COMPETITOR_ANALYSIS_SCRAPPER_CRON_HOST + "/api/competitor-analysis-scraper/start-cron");
+   // if(result.data.status=="KO") return NextResponse.json({errors:["Already running !"] },{status: 400});
     return NextResponse.json({data:["Running "]},{status: 200});
 }
--- app/competitor-analysis-scraper/api/cron/stop/route.ts
+++ app/competitor-analysis-scraper/api/cron/stop/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET (req:NextRequest){
-    const result = await axios.get(process.env.COMPETITOR_ANALYSIS_SCRAPPER_CRON_HOST + "/api/competitor-analysis-scraper/stop-cron");
-    if(result.data.status=="KO") return NextResponse.json({errors:["Already Stop !"] },{status: 400});
+    //const result = await axios.get(process.env.COMPETITOR_ANALYSIS_SCRAPPER_CRON_HOST + "/api/competitor-analysis-scraper/stop-cron");
+    //if(result.data.status=="KO") return NextResponse.json({errors:["Already Stop !"] },{status: 400});
     return NextResponse.json({data:["Stop successfully"]  },{status: 200});
 }
